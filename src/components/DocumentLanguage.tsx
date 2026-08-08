258d282a1328f1278eeccfffbb8c3f6ce541a7cd
Rustam Nigmatulin <rustam280897@gmail.com>
2026-08-08 18:04:21 +0300

    Add firebase/app package

--- src/components/DocumentLanguage.tsx
+++ src/components/DocumentLanguage.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
-/** Keeps `<html lang>` in sync with the active i18next locale. */
 export function DocumentLanguage() {
   const { i18n } = useTranslation()
 

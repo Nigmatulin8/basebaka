53a5acee10d6cd421b6065c618b197c815cf8e0a
Rustam Nigmatulin <rustam280897@gmail.com>
2026-08-16 19:24:02 +0300

    lint fix

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,22 +1,8 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { AppHeader } from '@/components/auth/AppHeader.tsx'
-import { SideNav } from '@/components/layout/SideNav.tsx'
+import { createFileRoute } from '@tanstack/react-router'
+import { AppLayout } from '@/components/layout/AppLayout.tsx'
 import { requireAuthentication } from '@/router/auth-guards.ts'
-import '@/components/auth/styles.scss'
 
 export const Route = createFileRoute('/_app')({
   beforeLoad: ({ context }) => requireAuthentication(context),
   component: AppLayout,
 })
-
-function AppLayout() {
-  return (
-    <div className="flex min-h-svh bg-base text-ink">
-      <SideNav />
-      <div className="flex min-w-0 flex-1 flex-col">
-        <AppHeader />
-        <Outlet />
-      </div>
-    </div>
-  )
-}
